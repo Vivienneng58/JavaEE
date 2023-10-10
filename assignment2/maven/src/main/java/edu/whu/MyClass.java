3a3db4ac079fd98fd9443e3bc71f98f470b51d0e
Vivienneng58 <79915214+Vivienneng58@users.noreply.github.com>
2023-10-10 19:46:33 +0800

    Delete assignment2 directory

--- assignment2/maven/src/main/java/edu/whu/MyClass.java
+++
@@ -1,9 +0,0 @@
-package edu.whu;
-import org.example.InitMethod;
-
-public class MyClass {
-    @InitMethod
-    public void init() {
-        System.out.println("Initialization method is called.");
-    }
-}
